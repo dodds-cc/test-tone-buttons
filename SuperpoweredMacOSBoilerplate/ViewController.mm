8f98f0dd567d856e4fcd67c59a0f5e0649bd4d4a
Tom Dodds <thomas@creativelycomon.co.uk>
2021-11-04 10:35:13 +0100

    initial commit

---
+++ SuperpoweredMacOSBoilerplate/ViewController.mm
@@ -0,0 +1,89 @@
+//
+//  ViewController.m
+//  SuperpoweredMacOSBoilerplate
+//
+//  Created by Thomas Dodds on 2021. 10. 21..
+//
+
+#import "ViewController.h"
+#import "Superpowered.h"
+#import "SuperpoweredSimple.h"
+#import "SuperpoweredOSXAudioIO.h"
+#import "SuperpoweredGenerator.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) SuperpoweredOSXAudioIO *superpowered;
+@end
+
+
+
+@implementation ViewController {
+    Superpowered::Generator *generator;
+    float genVolume, genPreviousVolume;
+    __weak IBOutlet NSButton *hello;
+}
+
+// I would like this to be triggered on mouse down of all the blue buttons, passing in a float as a value for frequency.
+-(void)playNote:(float) frequency {
+    generator->frequency = frequency;
+    genVolume = 1;
+}
+
+// I would like this to be triggered on mouse up (and nouse out) of all buttons
+
+-(void)stopNote {
+    genVolume = 0;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    Superpowered::Initialize(
+     "ExampleLicenseKey-WillExpire-OnNextUpdate",
+     false, // enableAudioAnalysis (using SuperpoweredAnalyzer, SuperpoweredLiveAnalyzer, SuperpoweredWaveform or SuperpoweredBandpassFilterbank)
+     false, // enableFFTAndFrequencyDomain (using SuperpoweredFrequencyDomain, SuperpoweredFFTComplex, SuperpoweredFFTReal or SuperpoweredPolarFFT)
+     false, // enableAudioTimeStretching (using SuperpoweredTimeStretching)
+     true, // enableAudioEffects (using any SuperpoweredFX class)
+     true, // enableAudioPlayerAndDecoder (using SuperpoweredAdvancedAudioPlayer or SuperpoweredDecoder)
+     false, // enableCryptographics (using Superpowered::RSAPublicKey, Superpowered::RSAPrivateKey, Superpowered::hasher or Superpowered::AES)
+     false  // enableNetworking (using Superpowered::httpRequest)
+    );
+
+    // Do any additional setup after loading the view.
+    NSLog(@"Superpowered version: %u", Superpowered::Version());
+    
+    
+   
+    generator = new Superpowered::Generator(44100, Superpowered::Generator::Sine);
+    generator->frequency = 440.0;
+    genVolume = 1;
+    genPreviousVolume = 1;
+
+    self.superpowered = [[SuperpoweredOSXAudioIO alloc] initWithDelegate:(id<SuperpoweredOSXAudioIODelegate>)self preferredBufferSizeMs:12 numberOfChannels:2 enableInput:true enableOutput:true];
+    [self.superpowered start];
+}
+
+- (bool)audioProcessingCallback:(float **)inputBuffers inputChannels:(unsigned int)inputChannels outputBuffers:(float **)outputBuffers outputChannels:(unsigned int)outputChannels numberOfFrames:(unsigned int)numberOfFrames samplerate:(unsigned int)samplerate hostTime:(unsigned long long int)hostTime {
+    
+    float outputBuffer[numberOfFrames * 2];
+    
+    generator->generate(outputBuffers[0], numberOfFrames);
+    
+    Superpowered::Interleave(outputBuffers[0], outputBuffers[0], outputBuffer, numberOfFrames);
+    
+    Superpowered::Volume(
+        outputBuffer,
+        outputBuffer,
+        genPreviousVolume,
+        genVolume,
+        numberOfFrames
+    );
+    genPreviousVolume = genVolume;
+    
+    Superpowered::DeInterleave(outputBuffer, outputBuffers[0], outputBuffers[1], numberOfFrames);
+
+    return true;
+}
+
+@end
